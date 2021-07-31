10962002966912f431a9f976a1cc4ea2e3511833
vencax <vencax@centrum.cz>
2021-07-31 20:17:53 +0200

    fix

--- api/mailsend.js
+++ api/mailsend.js
@@ -1,24 +1,19 @@
 import nodemailer from 'nodemailer'
 
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASSWD
-  }
-})
-
-export default async function (mocks) {
+export default function (mocks) {
   if (mocks) return mocks.sendMail
 
-  console.log(`verifying STMP (${process.env.SMTP_CONN}) ...`)
-  await transporter.verify()
-  return function sendMail (message) {
-    return new Promise((resolve, reject) => {
-      transporter.sendMail(message, (err) => {
-        return err ? reject(err) : resolve()
-      })
+  return async function sendMail (message) {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWD
+      }
     })
+    console.log(`verifying STMP (${process.env.SMTP_CONN}) ...`)
+    await transporter.verify()
+    return transporter.sendMail(message)
   }
 
 }
